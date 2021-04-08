53361c42b1d569d6b469c459e694657cc2af9be7
Paul <isakura313@gmail.com>
2021-04-09 01:01:15 +0300

    adsad

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,12 @@ function toPug() {
     .pipe(pug())
     .pipe(gulp.dest("./src"));
 }
+function style(){
+  return gulp.src('./src/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
+}
 
 function watch() {
   browserSync.init({
@@ -21,8 +27,9 @@ function watch() {
   });
 }
 
-gulp.watch("./src/index.html").on("change", browserSync.reload);
-gulp.watch("./src/*.pug").on("change",gulp.series(toPug, browserSync.reload));
+gulp.watch('./src/sass/*.sass').on("change",gulp.series(style, browserSync.reload))
+gulp.watch("./src/index.html").on("change", browserSync.reload)
+gulp.watch("./src/*.pug").on("change",gulp.series(toPug, browserSync.reload))
 
 exports.watch = watch;
 
